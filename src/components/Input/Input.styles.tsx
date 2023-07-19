0d0da198c214ab9bfc9fa15042a72cda5b3ca404
okti-web <sergey.nuzhnyy@exactprosystems.com>
2023-07-19 15:17:15 +0400

    first attempt

---
+++ src/components/Input/Input.styles.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+import { Input } from '@mui/material';
+import { TTheme } from '../../App';
+
+export const InputRoot = styled(Input)(({ theme }: { theme: TTheme }) => ({
+  backgroundColor: theme.secondaryBGColor ?? '#3A4362',
+  // color: theme.background ?? '#000015',
+	color: 'white',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+	outline: 'none',
+	border: 'none',
+  '&.MuiInput-root::before': {
+    border: 'none',
+  }
+}))
